e5ba3fe23110a6051cf6197e589f76b4e85f32cf
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2015-03-20 16:39:08 -0700

    Added invalid url check

--- new-entry.php
+++ new-entry.php
@@ -3,18 +3,22 @@
   $number = strtoupper(trim($_REQUEST['number']));
   $section = strtoupper(trim($_REQUEST['section']));
   $email = trim($_REQUEST['email']);
+  $inv ="The requested section is either no longer offered at UBC Vancouver or is not being offered this session.";
 
   //make url
   $url = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=" . $name . "&course=" . $number . "&section=" . $section;
+  $html = file_get_contents($url);
 
-  if (!empty($_GET['name'])
-   && !empty($_GET['number'])
-   && !empty($_GET['section'])
-   && !empty($_GET['email'])
-   && filter_var($email, FILTER_VALIDATE_EMAIL)
-   && preg_match("/[A-Z][A-Z][A-Z][A-Z]?/",$name)
-   && preg_match("/\d\d\d[A-Z]?/",$number)
-   && preg_match("/([A-Z]|\d)([A-Z]|\d)([A-Z]|\d)/",$section)){
+  if (strpos($html, $inv) !== false){
+    header("Location: http://ethanw.ca/projects/ubc-space-scraper/failed.html");
+  } elseif (!empty($_GET['name'])
+      && !empty($_GET['number'])
+      && !empty($_GET['section'])
+      && !empty($_GET['email'])
+      && filter_var($email, FILTER_VALIDATE_EMAIL)
+      && preg_match("/^[A-Z]{3,4}$/",$name)
+      && preg_match("/^\d{3}[A-Z]?$/",$number)
+      && preg_match("/^([A-Z]|\d){3}$/",$section)){
 
     //open files and write
     $email_file = fopen("emails","a+");
