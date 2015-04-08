61e883d371715a01c612c7c795dda2c7101f89ea
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2015-04-08 15:50:46 -0700

    Added reliance on index

--- new-entry.php
+++ new-entry.php
@@ -3,21 +3,10 @@
   $number = strtoupper(trim($_REQUEST['number']));
   $section = strtoupper(trim($_REQUEST['section']));
   $email_num = trim($_REQUEST['emailnum']);
-  $term = strtoupper(trim($_REQUEST['term']));
+  $unsan_term = strtoupper(trim($_REQUEST['term']));
 
-  if ($term === "W"){
-    $year = "2014";
-  } else {
-    $year = "2015";
-  }
-
-
-  //if (!filter_var($email_num0, FILTER_VALIDATE_EMAIL) === false){
-    //$email_num = $email_num0;
-  //} else {
-    //$email_num = preg_replace("/[^0-9]/", "", $email_num0);
-  //}
-
+  $term = $unsan_term[0];
+  $year = substr($unsan_term,1);
 
   $inv ="The requested section is either no longer offered at UBC Vancouver or is not being offered this session.";
 
@@ -27,8 +16,6 @@
 
   fwrite(fopen("test","x+"), (!filter_var($email_num, FILTER_VALIDATE_EMAIL) === false) . preg_match("/^[0-9]{10}$/", $email_num));
 
-  //&& ((!filter_var($email_num, FILTER_VALIDATE_EMAIL) === false) || preg_match("/^[0-9]{10}$/", $email_num))
-  //preg_match("/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/",$email_num0);
 
   if (strpos($html, $inv) !== false){
     header("Location: http://ethanw.ca/projects/ubc-space-scraper/failed.html");
@@ -48,6 +35,7 @@
     fwrite($contacts_file, $email_num . "\n");
     fwrite($url_file, $url . "\n");
     fwrite($courses_file, $name . " " . $number . " " . $section . "\n");
+    mail("e@ethanw.ca", "Someone used seat4.me!");
     header("Location: http://ethanw.ca/projects/ubc-space-scraper/completed.html");
   } else {
     header("Location: http://ethanw.ca/projects/ubc-space-scraper/failed.html");
