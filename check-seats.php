216fa430308d1c40f0e1da4ce4424ec6a3dfd89f
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2017-08-26 15:20:56 -0700

    Use pushbullet to nofity instead of email (disgusting hack)

--- check-seats.php
+++ check-seats.php
@@ -6,6 +6,11 @@ unset($urls[sizeof($urls) - 1]);
 unset($emails_num[sizeof($emails_num) - 1]);
 unset($courses[sizeof($courses) - 1]);
 
+// Map of users to pushbullet api key. Emails not in this map will not receive messages.
+$data = array();
+$data["ethan.wilberforce@gmail.com"] = 'o.h7vQQXAcHfdejP7bhZdfjhsQa2069zga';
+$data["kellyleacarleton@gmail.com"] = 'o.wbUfOlk75wfRXPp9C1ZwlmhuNvtpbMhd';
+
 $headers = "From: openseat@seat4.me" . "\r\n" ;
 //Check through $urls array for classes with available spots
 for ($x = 0; $x < sizeof($urls); $x++) {
@@ -17,35 +22,12 @@ for ($x = 0; $x < sizeof($urls); $x++) {
   $b = explode("</strong>", $a[1])[0];
   $spots = intval($b);
 
-  //Check if spots > 0
   if ($spots > 0){
-    //if (!filter_var($emails_num[$x], FILTER_VALIDATE_EMAIL) === false){
-
-      //send email saying space is available
-    mail($emails_num[$x],
-      $courses[$x] . " has seats available!",
-      "Click the link to claim your seat.\n" . $urls[$x] . "\n\n\n" .
-      "Do not reply to this email, this is an unatended mailbox.",
-      $headers);
-
-    //Comented out code below and if statement above are SMS implementations
-    /*} else {
-			require_once 'plivo.php';
-      $auth_id = "MAMJA2NDA5MWNJOTKWYJ";
-      $auth_token = "NWI3ZWU0MGIyNTc1NWQyODc1ZGM5Yzg4OTk2ZTdl";
-
-      $p = new RestAPI($auth_id, $auth_token);
-
-      // Send a message
-      $params = array(
-              'src' => '16044250969',
-              'dst' => "1" . $emails_num[$x],
-              'text' => 'There is space in ' . $courses[$x] . '. Hurry! Click here to claim your seat! ' . $urls[$x],
-              'method' => 'POST'
-          );
-      // Send message
-      $response = $p->send_message($params);
-    }*/
+    // send pushbullet if user is registered in the map
+    if (!empty($data[$emails_num[$x]])) {
+      $cmd = "./../../../../../../../../../../../../var/www/ethanw.ca/projects/ubc-space-scraper/sendPushbullet.sh " . $data[$emails_num[$x]] . ' "' . $courses[$x] . '" ' . $urls[$x] . ' > log';
+      shell_exec($cmd);
+    }
 
     $tot = explode("\n", fread(fopen("/var/www/ethanw.ca/projects/ubc-space-scraper/total","r"), 34217728));
     fwrite(fopen("/var/www/ethanw.ca/projects/ubc-space-scraper/total","w"),$tot[0] + 1);
@@ -69,19 +51,13 @@ for ($x = 0; $x < sizeof($urls); $x++) {
 
     //Write unused data to files
     foreach ($urls as $url){
-      if (!($urls[$x] == $url)){
         fwrite(fopen("/var/www/ethanw.ca/projects/ubc-space-scraper/urls","a+"), $url . "\n");
-      }
     }
     foreach ($emails_num as $email_num){
-      if (!($emails_num[$x] == $email_num)){
         fwrite(fopen("/var/www/ethanw.ca/projects/ubc-space-scraper/contacts","a+"), $email_num . "\n");
-      }
     }
     foreach ($courses as $course){
-      if (!($courses[$x] == $course)){
         fwrite(fopen("/var/www/ethanw.ca/projects/ubc-space-scraper/courses","a+"), $course . "\n");
-      }
     }
   }
 }
---
+++ sendPushbullet.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+
+
+
+
+A='{"body":" Click the link to claim your seat.\n'
+B='","title":"'
+C=' has seats available!","type":"link","url":"'
+E='"}'
+D=$A$B$2$C$3$E
+
+curl --header "Access-Token: $1" \
+     --header 'Content-Type: application/json' \
+     --data-binary  "$D" \
+     --request POST \
+     https://api.pushbullet.com/v2/pushes
